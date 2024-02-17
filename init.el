2aa168c19636fc63dbf46172479e2f082570ed45
jlmg <jlmg3543@gmail.com>
2024-02-16 23:24:25 -0600

    2024-02-16

--- init.el
+++ init.el
@@ -1,16 +1,2 @@
 (setq vc-follow-symlinks t) ; yeah...
 (org-babel-load-file "~/.emacs.d/config.org")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(initial-frame-alist '((fullscreen . maximized)))
- '(package-selected-packages
-   '(org-modern web-mode vterm undo-tree undo-propose tab-bar-buffers swiper rainbow-mode pulsar poet-theme org-view-mode org-superstar org-download org-cliplink org-bullets mood-line markdown-mode lua-mode lorem-ipsum go-mode feebleline emojify emmet-mode doom-themes doom-modeline company bufferlo beacon ample-theme all-the-icons)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
